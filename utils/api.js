614f30de2f575751af62e5ce2054345a386ff2e8
Sparsh S. Pradhan <sparshs.pradhan@192.168.1.4>
2025-03-18 00:43:30 +0530

    update

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const localurl="http://localhost:5008/transactions"
 const vercelurl="https://finance-backend-sparsh.vercel.app/transactions"
 const api = axios.create({
-  baseURL: "https://finance-backend-sparsh.vercel.app/transactions",
+  baseURL: "https://finance-backend-sparsh.vercel.app",
 });
 
 export default api;
